c86ef8a1a76ad7d21847e3ec0ed89300215ed031
Icebytesize <Matthiasuk@gmail.com>
2026-09-03 11:52:10 +0200

    branch med karakter classer i

---
+++ src/DnD/Classes/Character.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Interfaces;
+public abstract class Character: IDamageable
+{
+	int HP { get; private set; }
+	int MaxHP { get; private set; }
+	string Name { get; private set; } = string.Empty;
+	int Level { get; private set; }
+	int Xp { get; private set; }
+	int AttackPower { get; private set; }
+
+	public abstract Character(string name, int level, int maxHP, int attackPower)
+    {
+        Name = name;
+        Level = level;
+        MaxHP = maxHP;
+        HP = MaxHP;
+        Xp = 0;
+        AttackPower = attackPower;
+    }
+
+
+
+
+
+}
