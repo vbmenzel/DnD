6a707c51550d98cb7f2d4d2542c9ad263ae7f9fb
vbmenzel <viktormenzel26@gmail.com>
2026-09-03 11:01:20 +0200

    Added IDamageable

---
+++ src/DnD/IDamageable.cs
@@ -0,0 +1,15 @@
+namespace DnD;
+
+/// <summary>Defines the contract for entities that can receive and take damage.</summary>
+public interface IDamageable
+{
+    /// <summary>Gets the current health points of the entity.</summary>
+    int CurrentHealth { get; }
+
+    /// <summary>Gets a value indicating whether the entity has been defeated.</summary>
+    bool IsDefeated { get; }
+
+    /// <summary>Applies damage to the entity, reducing its current health.</summary>
+    /// <param name="amount">The amount of damage to apply.</param>
+    void TakeDamage(int amount);
+}
