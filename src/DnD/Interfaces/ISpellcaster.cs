e1c2a271757d2f12e6816c16520868dcfeff3468
vbmenzel <viktormenzel26@gmail.com>
2026-09-03 14:40:03 +0200

    Added XML docs to ISpellcaster

--- src/DnD/Interfaces/ISpellcaster.cs
+++ src/DnD/Interfaces/ISpellcaster.cs
@@ -1,8 +1,18 @@
 namespace DnD.Interfaces;
 
+/// <summary>
+/// Defines the contract for characters that can cast spells.
+/// </summary>
 public interface ISpellcaster
 {
+    /// <summary>
+    /// Gets the spellcaster's current mana points.
+    /// </summary>
     int CurrentMana { get; }
 
+    /// <summary>
+    /// Casts a spell at the specified target.
+    /// </summary>
+    /// <param name="target">The target affected by the spell.</param>
     void CastSpell(IDamageable target);
 }
