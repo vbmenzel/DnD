8d7b3a8bb3ab007b34e7661218d272bfdbcee8e7
Halime Ekiz Sen <halime_sen88@hotmail.com>
2026-09-03 12:28:10 +0200

    Add item hierarchy and party structure

---
+++ src/DnD/Items/Potion.cs
@@ -0,0 +1,16 @@
+﻿namespace DnD.Items
+{
+	public class Potion : Item
+	{
+		public int HealAmount { get; private set; }
+
+		public Potion(string name, int healAmount)
+			: base(name)
+		{
+			HealAmount = healAmount;
+		}
+
+		// TODO: Tilføj Use(Character target), når Character fra Person 1 er merged.
+		// Use skal kalde target.Heal(HealAmount).
+	}
+}
---
+++ src/DnD/Party/Party.cs
@@ -0,0 +1,10 @@
+﻿namespace DnD.Party
+{
+	public class Party
+	{
+		// TODO: Afventer Character-klassen fra Person 1.
+		// Party skal indeholde en List<Character>.
+		// AddMember(Character character) og
+		// RemoveMember(Character character) tilføjes efter merge.
+	}
+}
