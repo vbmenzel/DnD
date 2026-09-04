d2abaa396570572a6a1651eae1334b8f54ae2791
vbmenzel <viktormenzel26@gmail.com>
2026-09-04 11:27:07 +0200

    Added dictionary equipment slots

---
+++ src/DnD/Items/EquipmentSlot.cs
@@ -0,0 +1,17 @@
+namespace DnD.Items;
+
+/// <summary>
+/// Identifies a slot that can contain one equipped item.
+/// </summary>
+public enum EquipmentSlot
+{
+    /// <summary>
+    /// The slot used by a weapon.
+    /// </summary>
+    Weapon,
+
+    /// <summary>
+    /// The slot used by armor.
+    /// </summary>
+    Armor,
+}
